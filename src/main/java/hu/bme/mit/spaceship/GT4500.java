7669a8f2608b596fa4df6b646446758686ef4b8f
Kacso Peter <kacso.pepe@yahoo.com>
2021-04-12 15:09:47 +0300

    Update comment on GT4500 to recommend for new owners

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -1,7 +1,7 @@
 package hu.bme.mit.spaceship;
 
 /**
-* A simple spaceship with two proton torpedo stores and four lasers
+* A simple spaceship with two proton torpedo stores and four lasers. It is a good choice for new ship owners.
 */
 public class GT4500 implements SpaceShip {
 
