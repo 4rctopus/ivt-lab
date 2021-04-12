0319bcad6b9e7176125d8f253dc234e76e1c59f8
Kacso Peter <kacso.pepe@yahoo.com>
2021-04-12 14:41:48 +0300

    Implement FiringMode.ALL in GT4500 spaceship

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,7 +78,10 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
+        boolean primaryTorpedoSuccess = primaryTorpedoStore.fire(1);
+        boolean secondaryTorpedoSuccess = secondaryTorpedoStore.fire(1);
+
+        firingSuccess = primaryTorpedoSuccess || secondaryTorpedoSuccess;
 
         break;
     }
